00e0143ff12e2897f0354db5dd9179435f66cc96
Quatn <nguyenminhquanvh@gmail.com>
2024-07-18 08:06:48 +0700

    Added thumbnail to blog + indented database.json (all 6000 lines)

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,36 +1,44 @@
-import React, { useContext, useEffect } from 'react';
-import {Row, Col, Button} from 'react-bootstrap'
-import Carousel from 'react-bootstrap/Carousel';
-import './css/HomePage.css'
+import React, { useContext, useEffect } from "react";
+import { Row, Col, Button, Container } from "react-bootstrap";
+import Carousel from "react-bootstrap/Carousel";
+import "./css/HomePage.css";
 
 function HomePage() {
   return (
-    <Carousel>
-      <Carousel.Item>
-        <div className='slider'><img src="./images/Promotional1.png" /></div>
-	  	{/*<Carousel.Caption>
+    <Container className="mt-3">
+      <Carousel>
+        <Carousel.Item>
+          <div className="slider">
+            <img src="./images/Promotional1.png" />
+          </div>
+          {/*<Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>*/}
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className='slider'><img src="./images/Promotional2.jpg" /></div>
-	  {/*<Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="slider">
+            <img src="./images/Promotional2.jpg" />
+          </div>
+          {/*<Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>*/}
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className='slider'><img src="./images/Promotional3.jpg" /></div>
-	  {/*<Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="slider">
+            <img src="./images/Promotional3.jpg" />
+          </div>
+          {/*<Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
         </Carousel.Caption>*/}
-      </Carousel.Item>
-    </Carousel>
+        </Carousel.Item>
+      </Carousel>
+    </Container>
   );
 }
 
-export default HomePage
+export default HomePage;
